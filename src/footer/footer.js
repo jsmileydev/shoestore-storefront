0a0d22e4b0de30db90bdd721e7292d2eeeacfb36
unknown <jsmileydev@gmail.com>
2019-07-01 17:40:01 -0500

    Footer grid styling

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './footer.css';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
@@ -14,73 +15,73 @@ class Footer extends React.Component {
 			<footer>
 				<Col>
 					<Row className="footer-btn-row justify-content-center m-5">
-						<Button className="footer-btn mx-5" variant="secondary">
-							Find a Location 
-                            <FontAwesomeIcon icon={faMapMarkerAlt} className="d-block mx-auto" />
+						<Button className="footer-btn mx-4 my-3" variant="secondary">
+							Find a Location
+							<FontAwesomeIcon icon={faMapMarkerAlt} className="d-block mx-auto" />
 						</Button>
-						<Button className="footer-btn mx-5" variant="secondary">
-							Contact Us 
-                            <FontAwesomeIcon icon={faMobileAlt} className="d-block mx-auto" />
+						<Button className="footer-btn mx-4 my-3" variant="secondary">
+							Contact Us
+							<FontAwesomeIcon icon={faMobileAlt} className="d-block mx-auto" />
 						</Button>
-						<Button className="footer-btn mx-5" variant="secondary">
-							Track Your Order 
-                            <FontAwesomeIcon icon={faShippingFast} className="d-block mx-auto" />
+						<Button className="footer-btn mx-4 my-3" variant="secondary">
+							Track Your Order
+							<FontAwesomeIcon icon={faShippingFast} className="d-block mx-auto" />
 						</Button>
 					</Row>
-				</Col>
-				<Col>
-					<Row className="m-5">
-						<Form className="signup" as={Col} sm={10} md={6}>
-							<Form.Row className="align-items-end">
-								<Col className="px-0" sm={8}>
-									<Form.Group as={Col} controlId="formGridEmail" className="p-0 mb-0">
-										<Form.Label>Sign up for sales and updates!</Form.Label>
-										<Form.Control
-											type="email"
-											placeholder="Enter email address"
-											className="email-input pb-"
-										/>
-									</Form.Group>
-								</Col>
-								<Col className="px-0" sm={4}>
-									<Button variant="info" type="submit" className="signup-btn btn-sm mt-4 py-0 h-50">
-										Submit
-									</Button>
-								</Col>
-							</Form.Row>
-						</Form>
-					</Row>
-					<Row className="m-5">
-						<div className="col-mdtext-left">
-							<p className="h6">Follow us on</p>
-							<div className="share-icons mr-2">
-								<img
-									src={require("./images/icons8-facebook-64.png")}
-									width="36"
-									alt="Twitter icon"
-								/>
-								<img
-									src={require("./images/icons8-twitter-64.png")}
-									width="36"
-									alt="Facebook icon"
-								/>
-								<img
-									src={require('./images/icons8-instagram-64.png')}
-									width="36"
-									alt="Instagram icon"
-								/>
-								<img
-									src={require("./images/icons8-youtube-64.png")}
-									width="36"
-									alt="Youtube icon"
-								/>
-								<img
-									src={require("./images/icons8-share-64.png")}
-									width="36"
-									alt="Share icon"
-								/>
+					<Row>
+						<Col sm={10} md={6} className="mx-auto">
+							<Form className="signup m-5">
+								<Form.Row className="align-items-end">
+									<Col className="px-0" sm={8}>
+										<Form.Group as={Col} controlId="formGridEmail" className="p-0 mb-0">
+											<Form.Label>Sign up for sales and updates!</Form.Label>
+											<Form.Control
+												type="email"
+												placeholder="Enter email address"
+												className="email-input pb-"
+											/>
+										</Form.Group>
+									</Col>
+									<Col className="px-0" sm={4}>
+										<Button
+											variant="info"
+											type="submit"
+											className="signup-btn btn-sm mt-4 py-0 h-50"
+										>
+											Submit
+										</Button>
+									</Col>
+								</Form.Row>
+							</Form>
+						</Col>
+						<Col sm={10} md={6} >
+							<div className="social-links text-center m-5">
+								<p className="h6">Follow us on</p>
+								<div className="share-icons mr-2">
+									<img
+										src={require('./images/icons8-facebook-64.png')}
+										width="36"
+										alt="Twitter icon"
+									/>
+									<img
+										src={require('./images/icons8-twitter-64.png')}
+										width="36"
+										alt="Facebook icon"
+									/>
+									<img
+										src={require('./images/icons8-instagram-64.png')}
+										width="36"
+										alt="Instagram icon"
+									/>
+									<img
+										src={require('./images/icons8-youtube-64.png')}
+										width="36"
+										alt="Youtube icon"
+									/>
+									<img src={require('./images/icons8-share-64.png')} width="36" alt="Share icon" />
+								</div>
 							</div>
-						</div>
+						</Col>
 					</Row>
 				</Col>
 				<div className="bg-dark border-dark">
